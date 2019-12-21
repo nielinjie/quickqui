6734c03ac077b71988e3687caa0bf16f75d9fa3b
nielinjie <nielinjie@gmail.com>
2019-12-21 15:27:29 +0800

    buildingContext

--- src/model/ModelDefine.ts
+++ src/model/ModelDefine.ts
@@ -1,40 +1,36 @@
 import { ValidateError } from "./ModelValidator";
 import { Model } from "./Model";
 
-import * as t from 'io-ts'
+import * as t from "io-ts";
 import { ModelWeaver } from ".";
 import { modelWeaverRT } from "./ModelWeaver";
 
-
 export interface ModelDefineConfig {
-    name: string;
-    filePattern: string;
-    extend: Promise<any>;
+  name: string;
+  filePattern: string;
+  extend: Promise<any>;
 }
 
-
-
-
 export interface ModelDefine {
-    name: string;
-    filePattern: string;
-    validatePiece(piece: any, model: Model): ValidateError[]
-    merge(model: Model, piece: any): Model
-    validateAfterMerge(model: Model): ValidateError[]
-    validateAfterWeave(model: Model): ValidateError[]
-    weavers:ModelWeaver[]
+  name: string;
+  filePattern: string;
+  validatePiece(
+    piece: any,
+    model: Model,
+    buildingContext?: any
+  ): ValidateError[];
+  merge(model: Model, piece: any, buildingContext?: any): Model;
+  validateAfterMerge(model: Model): ValidateError[];
+  validateAfterWeave(model: Model): ValidateError[];
+  weavers: ModelWeaver[];
 }
 
 export const modelDefineRuntimeType = t.type({
-    name: t.string,
-    filePattern: t.string,
-    validatePiece: t.Function,
-    merge: t.Function,
-    validateAfterMerge: t.Function,
-    validateAfterWeave: t.Function,
-    weavers:t.array(modelWeaverRT),
-})
-
-
-
-
+  name: t.string,
+  filePattern: t.string,
+  validatePiece: t.Function,
+  merge: t.Function,
+  validateAfterMerge: t.Function,
+  validateAfterWeave: t.Function,
+  weavers: t.array(modelWeaverRT)
+});
