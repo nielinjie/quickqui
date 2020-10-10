0da7d50d526d45ee75fd988975377b9be17182cf
nielinjie <nielinjie@gmail.com>
2020-10-10 15:06:56 +0800

    init

---
+++ src/util.ts
@@ -0,0 +1,27 @@
+export function notNil<TValue>(
+  value: TValue | null | undefined
+): value is TValue {
+  return value !== null && value !== undefined;
+}
+export function replaceInObject(
+  obj: any,
+  find: RegExp,
+  replace: (found: string) => any
+) {
+  const ret: any = {};
+  Object.keys(obj).forEach((key) => {
+    if (obj[key] && typeof obj[key] === "string" && find.test(obj[key])) {
+      ret[key] = replace(obj[key]);
+    } else {
+      ret[key] = obj[key];
+    }
+  });
+  return ret;
+}
+export function filterObject(obj: any) {
+  const ret: any = {};
+  Object.keys(obj)
+    .filter((key) => obj[key] !== undefined)
+    .forEach((key) => (ret[key] = obj[key]));
+  return ret;
+}
