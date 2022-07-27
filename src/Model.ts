badaa347d59d9994992dde0d6f8a59a013ccc710
nielinjie <nielinjie@gmail.com>
2022-07-27 10:50:34 +0800

    merge app-server

--- src/Model.ts
+++
@@ -1,8 +0,0 @@
-import { Model } from "@quick-qui/model-core";
-import { env } from "./Env";
-import axios from "axios";
-import { waitForUrlPort } from "./Util";
-
-export const model: Promise<Model> = waitForUrlPort(env.modelUrl).then(_ =>
-  axios.get(`${env.modelUrl}/models/default`).then(_ => _.data)
-);
